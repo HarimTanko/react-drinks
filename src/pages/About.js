032cc027219a39d7c454de40247d41ebadb1c556
Harim Tanko <harimtanko@gmail.com>
2022-03-02 19:04:52 +0000

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quidem ea
+        id, commodi, modi harum provident, autem fugiat ipsa itaque ad dolore
+        beatae optio quos temporibus laboriosam! Impedit dicta beatae modi
+        corporis? Deleniti nemo non odio, fuga aut molestiae modi aliquid
+        voluptas laboriosam quia nulla vitae similique deserunt laudantium
+        voluptatibus.
+      </p>
+    </section>
+  );
+};
+
+export default About;
